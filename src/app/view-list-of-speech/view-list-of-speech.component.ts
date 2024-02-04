b9683cc56035f5bfb99590927b37f9feff20481f
ricsie_kasim <ricsie.a@cvlinens.com>
2024-02-04 12:00:46 +0800

    delete

--- src/app/view-list-of-speech/view-list-of-speech.component.ts
+++ src/app/view-list-of-speech/view-list-of-speech.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { SpeechService } from '../speech.service';
 import { map, tap } from 'rxjs';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-view-list-of-speech',
@@ -8,6 +10,8 @@ import { map, tap } from 'rxjs';
   styleUrls: ['./view-list-of-speech.component.css']
 })
 export class ViewListOfSpeechComponent {
+  
+  @ViewChild('modalContent') modalContent?: ElementRef<any>;
 
   listOfSpeech: any[] = [];
   selectedData: any;
@@ -15,10 +19,19 @@ export class ViewListOfSpeechComponent {
   keywordsList = [
     {keywords: ''}
   ]
+  title: string = '';
 
-  constructor(private speechService: SpeechService) {}
+  constructor(
+    private speechService: SpeechService,
+    private modalService: NgbModal,
+    private router: Router
+  ) {}
 
   ngOnInit() {
+    this.getSpeechList()
+  }
+
+  getSpeechList() {
     this.speechService.getSpeechList()
       .subscribe(res => {
         this.listOfSpeech = res.speeches;
@@ -27,6 +40,7 @@ export class ViewListOfSpeechComponent {
         }
       })
   }
+
   selectTitle(data: any): void {
     this.selectedData = data;
     this.selectedData.keywords.map((text: any) => {
@@ -34,4 +48,30 @@ export class ViewListOfSpeechComponent {
       this.keywordsList.push({keywords: this.keywords})
     })
   }
+  
+  openDelete(): void {
+    if (this.selectedData) {
+      const modalRef = this.modalService.open(this.modalContent, { size: 'lg' });
+      this.title = this.selectedData.title;
+
+  }
+  }
+
+  delete() {
+    if (this.selectedData) {
+      const indexToDelete = this.listOfSpeech.findIndex(item => item.id === this.selectedData.id);
+  
+      if (indexToDelete !== -1) {
+        this.listOfSpeech.splice(indexToDelete, 1);
+        this.modalService.dismissAll(); // Close the modal
+  
+        // Automatically select the next item if available
+        const nextIndex = indexToDelete < this.listOfSpeech.length ? indexToDelete : indexToDelete - 1;
+        this.selectTitle(this.listOfSpeech[nextIndex]);
+      }
+    }
+  }
+  
+  
 }
+
